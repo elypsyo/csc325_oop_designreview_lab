7d634485ca00819ee4e34fbc0bf43488abd1bc06
armanmodiri <armanmod@buffalo.edu>
2022-03-01 14:52:03 -0500

    Made the Student class extend Human. Will now work on Fixing the resulting Errors.

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -9,11 +9,22 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Human
+public class Student extends Human{
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public void setAddress(String address) {
+
+    }
+
+    // ToDo 1: Make this class a child of Human
 	
 	// ToDo 2: Fix the resulting errors
-	
+
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
 	// ToDo 4: Add comments to your code
